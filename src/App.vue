e231238e563da605897aa7d83946c896ac980a8a
Eleandro Duzentos <hi@e200.dev>
2021-09-11 23:09:18 +0100

    feat: added home-header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,50 @@
 <template>
   <main>
-    <img src="./assets/github.png" alt="Github logo">
+    <div class="container">
+      <header class="home-header">
+        <img
+          class="home-header-logo"
+          src="./assets/github.png"
+          alt="Github logo"
+        />
 
-    <h1>Angolanos no Github</h1>
+        <h1 class="home-header-title">Angolanos no Github</h1>
+        <h2 class="home-header-subtitle">
+          Angolanos que contribuem para projectos Open-Source
+        </h2>
+        
+        We are 1610 developers based in Angola. Get to know us:
+      </header>
+    </div>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style>
-@import './styles/app.scss';
+<style lang="scss">
+@import "./styles/app.scss";
+
+.home {
+  &-header {
+    text-align: center;
+
+    &-logo {
+      margin-top: 35px;
+      max-width: 130px;
+    }
+    
+    &-title {
+      color: #1d1d1d;
+    }
+    
+    &-subtitle {
+      color: #5e5e5e;
+    }
+  }
+}
 </style>
