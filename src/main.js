c98c1b60888c375cf39850f05792b7755ebc4d15
Eleandro Duzentos <hi@e200.dev>
2021-09-19 22:39:23 +0100

    feat: added page title and favicon

--- src/main.js
+++ src/main.js
@@ -8,20 +8,17 @@ import store from './store'
 
 const app = createApp(App)
 
-app.use(store)
-
-const loadingImage = require('./assets/spinner.gif')
-
-app.use(VueGtag, {
-  config: { id: 'G-LGZ54YCVL5' }
-})
-
-app.use(VueLazyload, {
-  preLoad: 1.3,
-  // TODO: add error image
-  // error: errorimage,
-  loading: loadingImage,
-  attempt: 3,
-})
+app
+  .use(store)
+  .use(VueGtag, {
+    config: { id: 'G-LGZ54YCVL5' }
+  })
+  .use(VueLazyload, {
+    preLoad: 1.3,
+    // TODO: add error image
+    // error: errorimage,
+    loading: require('./assets/spinner.gif'),
+    attempt: 3,
+  })
 
 app.mount('#app')
